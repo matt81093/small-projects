ab99ab71d13395897332d0ff0de09b2d0d4fa6a2
matt81093 <matthew.kelley93+GitHub@gmail.com>
2021-10-24 04:39:49 +0000

    1st commit, imp based on Fireship's example, bug

---
+++ simple-note/src/components/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { onMount } from 'svelte'
+
+	let todos = []
+	let todoText = ''
+
+	onMount(() => {
+		const exisistingTodos = localStorage.getItem('todos')
+		todos = JSON.parse(exisistingTodos) || []
+	})
+
+	function addTodo() {
+		todos = [...todos, todoText]
+		localStorage.setItem('todos', JSON.stringify(todos))
+	}
+</script>
+
+<main>
+	<ul>
+		{#each todos as todo}
+			<li>{todos}</li>
+		{/each}
+	</ul>
+
+	<form on:submit|preventDefault={addTodo}>
+		<input bind:value={todoText}>
+		<input type="submit" value="Add todo">
+	</form>
+</main>
+
+<style>
+
+</style>
